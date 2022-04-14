81b37934abb71ddf1d19f390d72b41235dcf3942
Veronica Minciuna <veronicaminci@gmail.com>
2022-04-14 12:37:51 -0400

    item list component

---
+++ src/ItemList.js
@@ -0,0 +1,31 @@
+import {FaTrashAlt} from 'react-icons/fa'
+
+
+const ItemList = ({items, handleCheck,handleDelete}) => {
+  return (
+    <div>
+        <ul>
+            {items.map((item)=> (
+                <li className='item' key={item.id}>
+                    <input
+                    type="checkbox"
+                    onChange={() => handleCheck(item.id)}
+                    checked={item.checked}
+                    />
+                    <label
+                    style={(item.checked) ? {textDecoration: 'line-through'} : null}
+                    onDoubleClick={() => handleCheck(item.id)}
+                    >{item.item}</label>
+                    <FaTrashAlt 
+                    onClick={()=> handleDelete(item.id)}
+                    role="button" 
+                    tabIndex="0"
+                    />
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default ItemList
